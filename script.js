f8e2fdeca22a83bd628cdcce2c7270777074bce5
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2022-09-19 13:28:27 -0300

    Update script

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ let cardNumber_field = document.getElementById("inumber")
 let date_field = document.getElementById("idate")
 let cvc_field = document.getElementById("icvc")
 
-if(name_field.length == 0){
-    name_field.style.border = "1px solid hsl(0, 100%, 66%)"
-}
-
 function confirmButton(){
     if(form.style.display = "block"){
         form.style.display = "none"
